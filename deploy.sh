0129be9445e487dd8d3a7541682b87bf00ec6cac
ariel <alvarez.ariel.work@gmail.com>
2015-04-19 03:32:09 -0300

    Cambiado deploy target para que apunte a uqbar-project.github.io

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,12 @@ CURRENT_COMMIT=`git rev-parse HEAD`
 ORIGIN_URL="github.com/uqbar-project/uqbar-project.github.io.git"
 ORIGIN_URL_WITH_CREDENTIALS="https://${GITHUB_TOKEN}@${ORIGIN_URL}"
 
-echo "Cloning master site branch"
-git clone "ORIGIN_URL_WITH_CREDENTIALS" || exit 1
+echo "Compiling site"
+
+mkdir ${TEMP_DIRECTORY}
+harp compile ./site ${TEMP_DIRECTORY}
+cp .gitignore ${TEMP_DIRECTORY}
+ls ${TEMP_DIRECTORY}
 
 echo "Removing old static content"
 git rm -rf . || exit 1
@@ -23,8 +27,9 @@ git config user.email "travis@uqbar-project.com" || exit 1
 
 git add -A . || exit 1
 git commit --allow-empty -m "Generated static site for $CURRENT_COMMIT" || exit 1
+git remote add origin "$ORIGIN_URL_WITH_CREDENTIALS"
 #git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages > /dev/null 2>&1
-git push --force "$ORIGIN_URL_WITH_CREDENTIALS" master
+git push -u --force origin master
 
 echo "Cleaning up temp files"
 rm -Rf ${TEMP_DIRECTORY}
