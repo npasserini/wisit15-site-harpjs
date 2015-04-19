d9bf33d51cf0035bc7a1dd333d93f0c7b94e72f5
ariel <alvarez.ariel.work@gmail.com>
2015-04-19 03:49:01 -0300

    Cambiado deploy target para que apunte a uqbar-project.github.io

--- deploy.sh
+++ deploy.sh
@@ -12,25 +12,17 @@ echo "Compiling site"
 mkdir ${TEMP_DIRECTORY}
 harp compile ./site ${TEMP_DIRECTORY}
 cp .gitignore ${TEMP_DIRECTORY}
-ls ${TEMP_DIRECTORY}
 
-echo "Removing old static content"
-git rm -rf . || exit 1
+cd ${TEMP_DIRECTORY}
 
-echo "Copying newly generated static content"
-cp -r ${TEMP_DIRECTORY}/* . || exit 1
-cp ${TEMP_DIRECTORY}/.gitignore . || exit 1
-
-echo "Pushing new content to $ORIGIN_URL"
+git init
 git config user.name "Travis-CI" || exit 1
 git config user.email "travis@uqbar-project.com" || exit 1
-
-git add -A . || exit 1
+git add --all
 git commit --allow-empty -m "Generated static site for $CURRENT_COMMIT" || exit 1
-git remote set-url origin "$ORIGIN_URL_WITH_CREDENTIALS"
+git remote add origin "$ORIGIN_URL_WITH_CREDENTIALS"
+
 #git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages > /dev/null 2>&1
-git status
-ls -l
 git push -u --force origin master
 
 echo "Cleaning up temp files"
